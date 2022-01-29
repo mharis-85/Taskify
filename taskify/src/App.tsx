53272ce846660ccd70edaed2844a962ae0615c48
mharis0085 <mharis0085@gmail.com>
2022-01-30 01:09:57 +0500

    initial react setup

---
+++ taskify/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+// let name:string;
+// let role:[number, string]; //tuple
+
+// type Person={
+//   name:string;
+//   age?:number
+// }
+
+// let person:Person={
+//   name:'Haris'
+// }
+
+// let peoples:Person[]; //array of obj
+  
+function App() {
+  return (
+    <div className="App">
+      Hello
+    </div>
+  );
+}
+
+export default App;
