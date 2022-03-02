bae494b6e05fdc495c8840228d24e695798d7342
mharis0085 <mharis0085@gmail.com>
2022-03-03 00:32:20 +0500

    Todos list incomplete

--- taskify/src/App.tsx
+++ taskify/src/App.tsx
@@ -2,6 +2,8 @@ import React,{useState} from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import  InputField  from './components/InputField';
+import { Todo } from './model';
+import TodoList from './components/TodoList';
 
 // let name:string;
 // let role:[number, string]; //tuple
@@ -23,11 +25,21 @@ import  InputField  from './components/InputField';
 const App:React.FC=()=> {
 
   const [todo, setTodo]=useState<string>('');
+  const [todos, setTodos]=useState<Todo[]>([]);
+  const handleAdd=(e:React.FormEvent)=>{
+    e.preventDefault();
+    if(todo){
+      setTodos([...todos, {id:Date.now(), todo:todo , isDone:false}])
+    }
+    setTodo('');
+    
+  }
   console.log(todo);
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
